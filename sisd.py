2def7a1b130e82c03932f5b673396426c41c984e
Edsel Sulthan <edselfarrel206@gmail.com>
2026-04-04 14:20:24 +0700

    Delete sisd.py

--- sisd.py
+++
@@ -1,8 +0,0 @@
-# SISD: proses satu data per satu waktu
-numbers = [10, 20, 30, 40, 50]
-result = []
-
-for num in numbers:
-    result.append(num * 2)  # Satu instruksi, satu data
-
-print(result)  # [20, 40, 60, 80, 100]
